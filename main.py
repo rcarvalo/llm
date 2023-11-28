f5d605a1eaffba452225d95a2590cfe1c08ca046
Rémi Carvalot <103672484+rcarvalo@users.noreply.github.com>
2023-11-28 10:27:40 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.prompts import PromptTemplate
 from langchain.llms import CTransformers
 from langchain.document_loaders.csv_loader import CSVLoader  # using CSV loaders
-from langchain.document_loaders import DirectoryLoader
+from langchain.document_loaders import DirectoryLoader,PyPDFLoader
+from langchain.text_splitter import TokenTextSplitter
 
 import csv
 import tempfile 
@@ -33,7 +34,7 @@ from langchain.document_loaders.xml import UnstructuredXMLLoader
 from langchain.document_loaders.csv_loader import CSVLoader
 # Define a dictionary to map file extensions to their respective loaders
 loaders = {
-    '.pdf': PyMuPDFLoader,
+    '.pdf': PyPDFLoader,#PyMuPDFLoader,
     '.xml': UnstructuredXMLLoader,
     '.csv': CSVLoader,
     '.txt': TextLoader,
@@ -47,12 +48,7 @@ def create_directory_loader(file_type, directory_path):
         loader_cls=loaders[file_type],
     )
 
-
-
-
-
 def evaluate(csv_file,qa_chain,model_name,parameters):
-    
     with open(csv_file) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=';')
         line_count = 0
@@ -70,7 +66,6 @@ def generate_prompt(prompt: str, system_prompt: str = DEFAULT_SYSTEM_PROMPT) -> 
 
 {prompt} [/INST]
 """.strip()
-
 
 # Sidebar contents
 with st.sidebar:
@@ -92,51 +87,41 @@ def main():
     st.header("Chat with pdf")
 
     # upload a pdf file
-    pdf = st.file_uploader("Upload your PDF", type='csv')
+    pdf = st.file_uploader("Upload your PDF", type='pdf')
     
     #st.write(pdf)
     if pdf is not None:
-        #pdf reader
         
-
-        '''
-        pdf_reader = PdfReader(pdf)
-        
-        text = ""
-        for page in pdf_reader.pages:
-            text += page.extract_text()
-        '''
         with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
             tmp_file.write(pdf.getvalue())
             tmp_file_path = tmp_file.name
         print(pdf.name)
         #loader = CSVLoader(file_path=tmp_file_path, encoding="utf-8",csv_args={'delimiter': ','}) # any loader can be put here based on the data being used
         # Create DirectoryLoader instances for each file type
-        txt_loader = create_directory_loader('.txt', './txt_perfect')
+        #txt_loader = create_directory_loader('.txt', './txt_perfect')
         #pdf_loader = create_directory_loader('.pdf', './files')
         #xml_loader = create_directory_loader('.xml', './files')
         #csv_loader = create_directory_loader('.csv', './files')
 
         # Load the files
-        txt_documents = txt_loader.load()
+        #txt_documents = txt_loader.load()
         #csv_documents = csv_loader.load()
-        
-        
         #loader = DirectoryLoader('./files')#,glob="**/*.md", show_progress=True)
-        
-        text_splitter = RecursiveCharacterTextSplitter(
+        loader = PyPDFLoader(tmp_file_path)
+        pdf_documents = loader.load()
+        text_splitter = TokenTextSplitter(
             chunk_size=1024,
             chunk_overlap=64,
             #length_function=len
             )
-        '''
-        csv_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=200,
-            chunk_overlap=100,
-            length_function=len
-            )'''
+        
+        #csv_splitter = RecursiveCharacterTextSplitter(
+        #    chunk_size=200,
+        #    chunk_overlap=100,
+        #    length_function=len
+        #    )'''
         #split documents
-        chunks = text_splitter.split_documents(txt_documents)
+        chunks = text_splitter.split_documents(pdf_documents)
         #chunks.extend(csv_splitter.split_documents(csv_documents))
         # # embeddings
         store_name = pdf.name[:-4]
@@ -146,24 +131,18 @@ def main():
         if os.path.exists(f"{store_name}.pkl"):
             with open(f"{store_name}.pkl", "rb") as f:
                 VectorStore = pickle.load(f)
-            # st.write('Embeddings Loaded from the Disk')s
+            st.write('Embeddings Loaded from the Disk')
         else:
             embeddings = HuggingFaceInstructEmbeddings(
-                #model_name="hkunlp/instructor-large", model_kwargs={"device": DEVICE}$
-                model_name="BAAI/bge-large-en-v1.5", model_kwargs={"device": DEVICE}
+                model_name="hkunlp/instructor-large", model_kwargs={"device": DEVICE}
+                #model_name="BAAI/bge-large-en-v1.5", model_kwargs={"device": DEVICE}
             )
-            #embeddings = OpenAIEmbeddings()
-            #from_texts
             VectorStore = FAISS.from_documents(chunks, embedding=embeddings)
             with open(f"{store_name}.pkl", "wb") as f:
                 pickle.dump(VectorStore, f)
-        #VectorStore.save_local(DB_FAISS_PATH)
-        # embeddings = OpenAIEmbeddings()
-        #VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
- 
         # Accept user questions/query
         query = st.text_input("Ask questions about your PDF file:")
-        # st.write(query)
+        st.write(query)
  
         if query:
             model_name = 'dolphin-2.0-mistral-7b'
@@ -175,7 +154,7 @@ def main():
               'embedding':'hkunlp/instructor-large',
               'device':'MPS',
               'chunk_technique':'RecursiveTextSplitter'
-              }
+            }
             #docs = VectorStore.similarity_search(query=query, k=3)
             llm = CTransformers(
                 #model="TheBloke/dolphin-2.2.1-mistral-7B-GGUF",
@@ -184,9 +163,8 @@ def main():
                 model_type='mistral',
                 #model="TheBloke/Llama-2-13B-chat-GGUF",
                 #model_file="llama-2-13b-chat.Q4_0.gguf",
-                config={"max_new_tokens": 300, "context_length": 2048, "temperature": 0, "gpu_layers":1},
+                config={"max_new_tokens": 150, "context_length": 2048, "temperature": 0, "gpu_layers":1},
             )
-            #llm = OpenAI()
             chain = load_qa_chain(llm=llm, chain_type="stuff")
             
             SYSTEM_PROMPT = "Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer."
@@ -199,22 +177,27 @@ def main():
             """,
                 system_prompt=SYSTEM_PROMPT,
             )
+
+
             prompt = PromptTemplate(template=template, input_variables=["context", "question"])
+
+
+            
             chain = ConversationalRetrievalChain.from_llm(
                 llm=llm,
-                #chain_type="stuff",
+                chain_type="stuff",
                 retriever=VectorStore.as_retriever(search_kwargs={"k": 3}),
                 return_source_documents=True,
-                #chain_type_kwargs={"prompt": prompt},
+                combine_docs_chain_kwargs={"prompt": prompt},
             )
             #chain = ConversationalRetrievalChain.from_llm(llm, retriever=VectorStore.as_retriever())
-            #response = chain({'question':query, 'chat_history':[]})
-            evaluate('questionDataset.csv',chain,model_name,parameters)
+            response = chain({'question':query, 'chat_history':[]})
+            #evaluate('questionDataset.csv',chain,model_name,parameters)
             with open('parameter'+model_name+'.csv', 'w') as csv_file:
                 writer = csv.writer(csv_file)
                 for key, value in parameters.items():
                     writer.writerow([key, value])
-            #st.write(response)
+            st.write(response)
  
 if __name__ == '__main__':
     main()
